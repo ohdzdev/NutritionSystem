944f776df74727fb856d27b40b170270ad67a1e5
ColinKrist <colinkrist97@gmail.com>
2019-02-03 12:06:22 -0600

    automigrated loopback tables to mysql server

---
+++ zoo_api/server/automigrate.js
@@ -0,0 +1,13 @@
+// in case the mysql database is gone, after running the schema this will need to be ran
+// in order to create the tables from loopback
+
+// usage: node ./automigrate.js
+
+var server = require('./server');
+var ds = server.dataSources.zoo_mysql;
+var lbTables = ['account', 'ACL', 'RoleMapping', 'Role'];
+ds.automigrate(lbTables, function(er) {
+  if (er) throw er;
+  console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
+  ds.disconnect();
+});

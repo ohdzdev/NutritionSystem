42dc23e16fee7c29c1c9e475ec6e98fece688330
ColinKrist <colinkrist97@gmail.com>
2019-02-03 15:06:16 -0600

    add eslint and fix server side css rendering

---
+++ zoo_frontend/src/getPageContext.js
@@ -0,0 +1,49 @@
+import { SheetsRegistry } from 'jss';
+import { createMuiTheme, createGenerateClassName } from '@material-ui/core/styles';
+import primary from '@material-ui/core/colors/amber';
+// import secondary from '@material-ui/core/colors/indigo';
+
+// A theme with custom primary and secondary color.
+// It's optional.
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: primary['300'],
+      main: primary['500'],
+      dark: primary['700'],
+    },
+    type: 'light',
+  },
+  typography: {
+    useNextVariants: true,
+  },
+});
+
+function createPageContext() {
+  return {
+    theme,
+    // This is needed in order to deduplicate the injection of CSS in the page.
+    sheetsManager: new Map(),
+    // This is needed in order to inject the critical CSS.
+    sheetsRegistry: new SheetsRegistry(),
+    // The standard class name generator.
+    generateClassName: createGenerateClassName(),
+  };
+}
+
+let pageContext;
+
+export default function getPageContext() {
+  // Make sure to create a new context for every server-side request so that data
+  // isn't shared between connections (which would be bad).
+  if (!process.browser) {
+    return createPageContext();
+  }
+
+  // Reuse context on the client-side.
+  if (!pageContext) {
+    pageContext = createPageContext();
+  }
+
+  return pageContext;
+}

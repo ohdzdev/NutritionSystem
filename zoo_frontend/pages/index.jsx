e4920945bafcc6754067615e4a90edc334adcba2
Mitchell Slavik <Mitchell.A.Slavik@gmail.com>
2019-02-06 18:13:55 -0600

    Reorganize login page out of components

--- zoo_frontend/pages/index.jsx
+++ zoo_frontend/pages/index.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
-import Header from '../components/Header';
-import SignIn from '../components/SignIn';
 
 const Home = () => (
   <div>
-    <Header />
-    <div className="signin">
-      <SignIn className="signin" />
-    </div>
     <style jsx>
       {`
       .signin {

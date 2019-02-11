18b93fc3207e86f248d850a707f4bcf7c82aae62
Colin Krist <colinkrist97@gmail.com>
2019-02-11 08:39:16 -0600

    Update zoo_frontend/src/getPageContext.js
    Co-Authored-By: buffginger <andersonethan683@gmail.com>

--- zoo_frontend/src/getPageContext.js
+++ zoo_frontend/src/getPageContext.js
@@ -1,6 +1,5 @@
 import { SheetsRegistry } from 'jss';
 import { createMuiTheme, createGenerateClassName } from '@material-ui/core/styles';
-// import * as Colors from '@material-ui/core/colors';
 
 // import secondary from '@material-ui/core/colors/indigo';
 

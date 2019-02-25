d316f726d43c33184ccff4ca6dc09fd4825cd7ac
Colin Krist <colinkrist97@gmail.com>
2019-02-25 17:09:07 -0600

    remove ssl for now

--- zoo_frontend/env-config.js
+++ zoo_frontend/env-config.js
@@ -1,5 +1,5 @@
 const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  'process.env.BACKEND_URL': prod ? 'https://172.245.90.230:8080' : 'http://localhost:3333',
+  'process.env.BACKEND_URL': prod ? 'http://172.245.90.230:8080' : 'http://localhost:3333', //TODO change this back to https
 };

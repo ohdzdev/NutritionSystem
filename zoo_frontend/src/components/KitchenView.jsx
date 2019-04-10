3d7c0e62abc985e30f1bc9873f8c56946332b7b9
Ethan Anderson <andersonethan683@gmail.com>
2019-04-10 11:47:17 -0500

    UI for Kitchen page

---
+++ zoo_frontend/src/components/KitchenView.jsx
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import Typography from '@material-ui/core/Typography';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+import { withStyles } from '@material-ui/core/styles';
+
+
+let id = 0;
+function createData(unit, food) {
+  id += 1;
+  return {
+    unit, food,
+  };
+}
+
+const rows = [
+  createData('500 g', 'MIXED GREANS'),
+  createData('330 g', 'SEASONAL FRUIT'),
+];
+
+const styles = theme => ({
+  pageText: {
+    textAlign: 'right',
+    marginBottom: theme.spacing.unit,
+  },
+});
+
+
+class KitchenView extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      totalPages: 0,
+      currentPage: 0, // index of the data the user is on
+      data: [],
+    };
+  }
+
+  handleNext = () => {
+
+  }
+
+  handlePrev = () => {
+
+  }
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <div className={classes.pageText}>
+          <Typography> Page 1/23</Typography>
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+          <div style={{ flex: 1 }}>
+            <Typography style={{ fontSize: 30 }}>Procupine, Indian Crested</Typography>
+            <Typography style={{ fontSize: 22 }}>Abigail</Typography>
+            <Typography style={{ fontSize: 20, marginTop: 20 }}>Prep Notes</Typography>
+            <div>
+              <Typography>
+                  No shredded carrot.
+              </Typography>
+            </div>
+            <div>
+              <Typography style={{ fontSize: 20, marginTop: 20 }}>History</Typography>
+              <div>
+                <Typography>
+                  date: changes
+                </Typography>
+              </div>
+            </div>
+          </div>
+          <div style={{ flex: 1 }}>
+            <div style={{ textAlign: 'right' }}>
+              <Typography style={{ fontSize: 22 }}>Jungle</Typography>
+              <Typography style={{ fontSize: 22 }}>GIBBON</Typography>
+            </div>
+            <Table style={{ marginTop: 20 }}>
+              <TableBody>
+                {rows.map(row => (
+                  <TableRow key={row.id}>
+                    <TableCell align="left">{row.unit}</TableCell>
+                    <TableCell align="left">{row.food}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+KitchenView.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(KitchenView);
--- zoo_frontend/src/pages/kitchen/kitchen.jsx
+++ zoo_frontend/src/pages/kitchen/kitchen.jsx
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
+import Fab from '@material-ui/core/Fab';
 import Typography from '@material-ui/core/Typography';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
+import { withStyles } from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import NativeSelect from '@material-ui/core/NativeSelect';
+import NextIcon from '@material-ui/icons/NavigateNext';
+import PrevIcon from '@material-ui/icons/NavigateBefore';
 import Paper from '@material-ui/core/Paper';
+import KitchenView from '../../components/KitchenView';
+
 
 // import Link from 'next/link';
 // import { Button } from '@material-ui/core';
@@ -33,25 +35,12 @@ TabContainer.propTypes = {
   children: PropTypes.node.isRequired,
 };
 
-let id = 0;
-function createData(unit, food) {
-  id += 1;
-  return {
-    unit, food,
-  };
-}
-
-const rows = [
-  createData('500 g', 'MIXED GREANS'),
-  createData('330 g', 'SEASONAL FRUIT'),
-];
-
-
 export default class extends Component {
   static propTypes = {
-    // account: PropTypes.object.isRequired,
+    account: PropTypes.object.isRequired,
     token: PropTypes.string,
-    // classes: PropTypes.object.isRequired,
+    classes: PropTypes.object.isRequired,
+    FoodPrepTables: PropTypes.array.isRequired,
   };
 
   static defaultProps = {
@@ -73,44 +62,44 @@ export default class extends Component {
     const serverSpeciesAPI = new Species(authToken);
 
     const [
-      AllAnimals,
-      AllCaseNotes,
-      AllDeliveryContainers,
-      AllDietChanges,
-      AllDietHistory,
-      AllDietPlans,
-      AllDiets,
+      // AllAnimals,
+      // AllCaseNotes,
+      // AllDeliveryContainers,
+      // AllDietChanges,
+      // AllDietHistory,
+      // AllDietPlans,
+      // AllDiets,
       AllFoodPrepTables,
-      AllLifeStages,
-      AllPrepNotes,
-      AllSpecies,
+      // AllLifeStages,
+      // AllPrepNotes,
+      // AllSpecies,
     ] = await Promise.all([
-      serverAnimalAPI.getAnimals(),
-      serverCaseNotesAPI.getCaseNotes(),
-      serverDeliverContainersAPI.getDeliveryContainers(),
-      serverDietChangesAPI.getDietChanges(),
-      serverDietHistoryAPI.getDietHistories(),
-      serverDietPlansAPI.getDietPlans(),
-      serverDietsAPI.getDiets(),
+      // serverAnimalAPI.getAnimals(),
+      // serverCaseNotesAPI.getCaseNotes(),
+      // serverDeliverContainersAPI.getDeliveryContainers(),
+      // serverDietChangesAPI.getDietChanges(),
+      // serverDietHistoryAPI.getDietHistories(),
+      // serverDietPlansAPI.getDietPlans(),
+      // serverDietsAPI.getDiets(),
       serverFoodPrepTablesAPI.getFoodPrepTables(),
-      serverLifeStagesAPI.getLifeStages(),
-      serverPrepNotesAPI.getPrepNotes(),
-      serverSpeciesAPI.getSpecies(),
+      // serverLifeStagesAPI.getLifeStages(),
+      // serverPrepNotesAPI.getPrepNotes(),
+      // serverSpeciesAPI.getSpecies(),
     ]);
 
 
     return {
-      Animals: AllAnimals.data,
-      CaseNotes: AllCaseNotes.data,
-      DeliveryContainers: AllDeliveryContainers.data,
-      DietChanges: AllDietChanges.data,
-      DietHistory: AllDietHistory.data,
-      DietPlans: AllDietPlans.data,
-      Diets: AllDiets.data,
+      // Animals: AllAnimals.data,
+      // CaseNotes: AllCaseNotes.data,
+      // DeliveryContainers: AllDeliveryContainers.data,
+      // DietChanges: AllDietChanges.data,
+      // DietHistory: AllDietHistory.data,
+      // DietPlans: AllDietPlans.data,
+      // Diets: AllDiets.data,
       FoodPrepTables: AllFoodPrepTables.data,
-      LifeStages: AllLifeStages.data,
-      PrepNotes: AllPrepNotes.data,
-      Species: AllSpecies.data,
+      // LifeStages: AllLifeStages.data,
+      // PrepNotes: AllPrepNotes.data,
+      // Species: AllSpecies.data,
     };
   }
 
@@ -118,78 +107,55 @@ export default class extends Component {
     super(props);
     this.state = {
       asdf: props.token, // eslint-disable-line react/no-unused-state
-      value: 0,
+      table: 0,
+      labelWidth: 0,
     };
   }
 
-  handleTabChange = (event, value) => {
-    this.setState({ value });
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.value });
   };
-
 
   render() {
     // const { role } = this.props.account;
-    const { value } = this.state;
+    const { FoodPrepTables } = this.props;
+    console.log(this.state.table);
 
     return (
-      <div className={this.props.classes.root}>
-        <AppBar position="static" color="default">
-          <Tabs
-            value={value}
-            onChange={this.handleTabChange}
-            indicatorColor="primary"
-            textColor="primary"
-            variant="fullWidth"
-            centered
-          >
-            {/* value prop is the table_id of FOOD_PREP_TABLES, which is the same as table_code
-              These are hardcoded because not all entries in this table are being used. */}
-            <Tab label="Big Bag" value={1} />
-            <Tab label="Volunteer" value={2} />
-            <Tab label="Dome" value={3} />
-            <Tab label="Jungle" value={4} />
-            <Tab label="Meat" value={5} />
-            <Tab label="Daily" value={7} />
-          </Tabs>
-        </AppBar>
-
-        {/* Showing the same container, just changing the data in it on each tab change */}
-        <TabContainer>
-          <div style={{ display: 'flex', flexDirection: 'row' }}>
-            <div style={{ flex: 1 }}>
-              <Typography style={{ fontSize: 30 }}>Procupine, Indian Crested</Typography>
-              <Typography style={{ fontSize: 22 }}>Abigail</Typography>
-              <Typography style={{ fontSize: 20, marginTop: 20 }}>Prep Notes</Typography>
-              <div>
-                  No shredded carrot.
-              </div>
-              <div>
-                <Typography style={{ fontSize: 20, marginTop: 20 }}>History</Typography>
-                <div>
-                  date: changes
-                </div>
-              </div>
-            </div>
-            <div style={{ flex: 1 }}>
-              <div style={{ textAlign: 'right' }}>
-                <Typography style={{ fontSize: 22 }}>Jungle</Typography>
-                <Typography style={{ fontSize: 22 }}>GIBBON</Typography>
-              </div>
-              <Table style={{ marginTop: 20 }}>
-                <TableBody>
-                  {rows.map(row => (
-                    <TableRow key={row.id}>
-                      <TableCell align="left">{row.unit}</TableCell>
-                      <TableCell align="left">{row.food}</TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </div>
-
-          </div>
-        </TabContainer>
-
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+      }}
+      >
+        <div className={this.props.classes.root}>
+          <Fab variant="extended" aria-label="Previous" className={this.props.classes.fab}>
+            <PrevIcon className={this.props.classes.extendedIcon} />Prev
+          </Fab>
+          <FormControl className={this.props.classes.formControl}>
+            <InputLabel htmlFor="table-native-simple">Prep Table</InputLabel>
+            <NativeSelect
+              value={this.state.age}
+              onChange={this.handleChange('table')}
+              input={<Input name="table" id="table-native-helper" />}
+            >
+              <option value="" />
+              {/* value prop is the table_id of FOOD_PREP_TABLES */
+              FoodPrepTables ?
+                FoodPrepTables.map((item) => (
+                  <option value={item.tableId}>{item.description}</option>
+                ))
+                : null
+          }
+            </NativeSelect>
+          </FormControl>
+          <Fab variant="extended" color="secondary" aria-label="Next" className={this.props.classes.fab}>
+            Next<NextIcon className={this.props.classes.extendedIcon} />
+          </Fab>
+        </div>
+        <Paper className={this.props.classes.paper}>
+          <KitchenView />
+        </Paper>
       </div>
     );
   }

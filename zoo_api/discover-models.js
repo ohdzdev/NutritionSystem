eb461018b100194fcda8e2b33c006d3755d1b45c
Colin Krist <colinkrist97@gmail.com>
2019-04-10 19:00:15 -0500

    update schema of diet 'userlogin' to be mapped to the id of the new account table

--- zoo_api/discover-models.js
+++ zoo_api/discover-models.js
@@ -1,7 +1,18 @@
+// STEPS TO UPDATE LOOPBACK MODEL SCHEMA
+/**
+ * STEPS TO UPDATE LOOPBACK MODEL SCHEMA
+ * 1. in cmd set node environment to 'production'
+ *
+ * 2. change 'MODELNAME' to whatever table you want updated into loopback
+ *
+ * 3. save the file and run it via node 'node ./discover-models.js' if current dir is the same
+ */
 
 const loopback = require('loopback');
 const { promisify } = require('util');
 const fs = require('fs');
+
+require('dotenv').config();
 
 const writeFile = promisify(fs.writeFile);
 const readFile = promisify(fs.readFile);
@@ -9,6 +20,8 @@ const mkdirp = promisify(require('mkdirp'));
 
 const DATASOURCE_NAME = 'zoo_mysql';
 const dataSourceConfig = require('./server/datasources.production');
+
+const MODELNAME = 'DIETS'; // CHANGE ME WHEN UPDATING MODEL SCHEMA
 
 const db = new loopback.DataSource(dataSourceConfig[DATASOURCE_NAME]);
 async function discover() {
@@ -18,21 +31,21 @@ async function discover() {
   const options = { relations: true };
 
   // Discover models and relations
-  const WEEKDAYS = await db.discoverSchemas('WEEKDAYS', options);
+  const result = await db.discoverSchemas(MODELNAME, options);
 
   // Create model definition files
   await mkdirp('common/models');
-  console.log(WEEKDAYS);
+  console.log(result);
   await writeFile(
-    'common/models/WEEKDAYS.json',
-    JSON.stringify(WEEKDAYS['zoo.WEEKDAYS'], null, 2)
+    `common/models/${MODELNAME}.json`,
+    JSON.stringify(result[`zoo.${MODELNAME}`], null, 2)
   );
 
   // Expose models via REST API
   const configJson = await readFile('server/model-config.json', 'utf-8');
   console.log('MODEL CONFIG', configJson);
   const config = JSON.parse(configJson);
-  config.WEEKDAYS = { dataSource: DATASOURCE_NAME, public: true };
+  config[MODELNAME] = { dataSource: DATASOURCE_NAME, public: true };
   await writeFile(
     'server/model-config.json',
     JSON.stringify(config, null, 2)

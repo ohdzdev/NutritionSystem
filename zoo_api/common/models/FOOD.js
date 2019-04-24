2072d9c5a42ba4f4bb285f649eab746f84c81deb
Mitchell Slavik <Mitchell.A.Slavik@gmail.com>
2019-04-23 19:54:57 -0500

    Handle group_id being removed from subenclosures

--- zoo_api/common/models/FOOD.js
+++ zoo_api/common/models/FOOD.js
@@ -5,8 +5,6 @@ const Util = require('../../server/util');
 
 module.exports = function(Food) {
   Food.getDayPrepSheets = function(date, cb) {
-    console.log(date);
-
     const nextDate = moment(date, 'YYYY-MM-DD').add(1, 'days');
     app.datasources.zoo_mysql.connector.execute('CALL zoo.GetDayPrepSheets(?)', [nextDate.format('YYYY-M-D')], (err, [rows]) => {
       if (err) {

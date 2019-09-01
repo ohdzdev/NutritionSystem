08f65501786a1da6b7ba30e815002bde2ae1f754
Mitchell Slavik <Mitchell.A.Slavik@gmail.com>
2019-09-01 13:06:45 -0500

    Connect to cloud sql server

--- zoo_api/server/datasources.production.js
+++ zoo_api/server/datasources.production.js
@@ -4,14 +4,15 @@ module.exports = {
   zoo_mysql: {
     host: process.env.DB_HOST,
     port: 3306,
-    socketPath: process.env.INSTANCE_CONNECTION_NAME
-      ? `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`
-      : undefined,
+    socketPath:
+      process.env.NODE_ENV === 'production'
+        ? `/cloudsql/${process.env.SQL_INSTANCE_CONNECTION_NAME}`
+        : undefined,
     database: process.env.DB_NAME,
     password: process.env.DB_PASS,
-    name: "zoo_mysql",
-    connector: "mysql",
+    name: 'zoo_mysql',
+    connector: 'mysql',
     user: process.env.DB_USER,
-    all: true
-  }
+    all: true,
+  },
 };
--- zoo_api/server/server.js
+++ zoo_api/server/server.js
@@ -3,15 +3,15 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-const path = require("path");
-require("dotenv").config({
-  path: path.join(__dirname, "../.env")
+const path = require('path');
+require('dotenv').config({
+  path: path.join(__dirname, '../.env'),
 });
 
-const loopback = require("loopback");
-const boot = require("loopback-boot");
+const loopback = require('loopback');
+const boot = require('loopback-boot');
 
-const http = require("http");
+const http = require('http');
 
 const app = loopback();
 module.exports = app;
@@ -21,19 +21,19 @@ app.start = function() {
   server = http.createServer(app);
   server.listen(process.env.PORT || 8080, () => {
     const port = process.env.PORT || 8080;
-    const baseUrl = `http://${app.get("host")}:${port}`;
-    app.emit("started", baseUrl);
-    console.log("LoopBack server listening @ %s%s", baseUrl, "/");
-    if (app.get("loopback-component-explorer")) {
-      const explorerPath = app.get("loopback-component-explorer").mountPath;
-      console.log("Browse your REST API at %s%s", baseUrl, explorerPath);
+    const baseUrl = `http://${app.get('host')}:${port}`;
+    app.emit('started', baseUrl);
+    console.log('LoopBack server listening @ %s%s', baseUrl, '/');
+    if (app.get('loopback-component-explorer') && process.env.NODE_ENV !== 'production') {
+      const explorerPath = app.get('loopback-component-explorer').mountPath;
+      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
     }
   });
   return server;
 };
 
 // boot scripts mount components like REST API
-boot(app, __dirname, err => {
+boot(app, __dirname, (err) => {
   if (err) throw err;
   // start the server if `$ node server.js`
   if (require.main === module) {

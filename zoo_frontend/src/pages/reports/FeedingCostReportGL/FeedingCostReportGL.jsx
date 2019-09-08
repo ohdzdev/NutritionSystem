9830686773f992ac749e038666481e2d133bec68
Colin Krist <colinkrist97@gmail.com>
2019-09-07 22:46:03 -0500

    .

--- zoo_frontend/src/pages/reports/FeedingCostReportGL/FeedingCostReportGL.jsx
+++ zoo_frontend/src/pages/reports/FeedingCostReportGL/FeedingCostReportGL.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+// eslint-disable-next-line
 export default class DietCostReport extends Component {
   render() {
     return <div>hello world</div>;
--- zoo_frontend/src/pages/reports/reports.jsx
+++ zoo_frontend/src/pages/reports/reports.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+// eslint-disable-next-line
 export default class reports extends Component {
   render() {
     return <div>hello world put links to other reports here</div>;

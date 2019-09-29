5e9a58222d1f70532a692ef858e2b86d136e55bb
ColinKrist <colinkrist97@gmail.com>
2019-09-29 09:40:44 -0500

    pr feedback #1

--- zoo_frontend/src/pages/reports/index.js
+++
@@ -1,9 +0,0 @@
-import { compose } from 'recompose';
-
-import withAuth from '../../util/withAuth';
-
-import { Reports } from '../PageAccess';
-
-import page from './reports';
-
-export default compose(withAuth(Reports.roles))(page);
--- zoo_frontend/src/pages/reports/reports.jsx
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-
-// eslint-disable-next-line
-export default class reports extends Component {
-  render() {
-    return <div>hello world put links to other reports here</div>;
-  }
-}

6f8b6c370a1b7a24d8185dc35a14f3ffffc38f58
Colin Krist <colinkrist97@gmail.com>
2019-10-10 23:01:53 -0500

    fix dotenv with fb and nextjs

--- zoo_frontend/next.config.js
+++ zoo_frontend/next.config.js
@@ -1,8 +1,25 @@
+require('dotenv').config()
+
+const path = require('path')
+const Dotenv = require('dotenv-webpack')
+
 module.exports = {
   webpack: (config) => {
     config.node = {
       fs: 'empty',
     };
+
+    config.plugins = config.plugins || []
+
+    config.plugins = [
+      ...config.plugins,
+
+      // Read the .env file
+      new Dotenv({
+        path: path.join(__dirname, '.env'),
+        systemvars: true
+      })
+    ]
 
     return config;
   },
--- zoo_frontend/src/components/Firebase/firebase.js
+++ zoo_frontend/src/components/Firebase/firebase.js
@@ -3,26 +3,15 @@ import 'firebase/analytics';
 import 'firebase/performance';
 // import 'firebase/storage';
 
-// const firebaseConfig = {
-//   apiKey: process.env.FIREBASE_API_KEY,
-//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//   databaseURL: process.env.FIREBASE_DATABASE_URL,
-//   projectId: process.env.FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.FIREBASE_MSG_SEND_ID,
-//   appId: process.env.FIREBASE_APP_ID,
-//   measurementId: process.env.FIREBASE_MEASURE_ID,
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyA0x17OnTFISXMYUYIk2Mj60A1ougZtYaQ",
-  authDomain: "zoo-nutrition.firebaseapp.com",
-  databaseURL: "https://zoo-nutrition.firebaseio.com",
-  projectId: "zoo-nutrition",
-  storageBucket: "",
-  messagingSenderId: "867899608518",
-  appId: "1:867899608518:web:aa7930afdfea9bdd2bf6c7",
-  measurementId: "G-6P60DRKLBT"
+  apiKey: process.env.FB_API_KEY,
+  authDomain: process.env.FB_AUTH_DOMAIN,
+  databaseURL: process.env.FB_DB_URL,
+  projectId: process.env.FB_PROJ_ID,
+  storageBucket: process.env.FB_STOR_BUCKET,
+  messagingSenderId: process.env.FB_MSG_SEND_ID,
+  appId: process.env.FB_APP_ID,
+  measurementId: process.env.FB_MEASURE_ID,
 };
 
 
